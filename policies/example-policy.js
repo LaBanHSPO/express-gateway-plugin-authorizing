82ab8b95f3987a8eeaa37762e0e5edddad26e3f3
Ban Nguyen <bannv.sld@gmail.com>
2019-11-20 09:10:45 +0700

    update axios cache

--- policies/example-policy.js
+++ policies/example-policy.js
@@ -1,18 +1,25 @@
 // const requestify = require('requestify');
 // requestify.cacheTransporter(coreCacheTransporters.inMemory());
 
-const axios = require('axios');
+const axios = require('axios')
+const setupCache = require('axios-cache-adapter')
 
 module.exports = {
   name: 'authorizing',
   policy: (actionParams) => {
     return (req, res, next) => {
-      const baseURL = actionParams.baseUrl;
+      const baseURL = actionParams.baseUrl
+      // Create axios-cache-adapter instance
+      const cache = setupCache({
+        maxAge: 15 * 60 * 1000
+      })
+
       axios.create({
         timeout: 5000,
-      });
+        adapter: cache.adapter
+      })
 
-      const userInfo = actionParams.userInfo;
+      const userInfo = actionParams.userInfo
       axios({
         url: `${baseURL}/${userInfo}`,
         method: 'POST',
@@ -20,13 +27,13 @@ module.exports = {
           Authorization: req.headers.authorization
         }
       })
-      .then(response => {
-        req.user = response.data;
-        next();
-      })
-      .catch(error => {
-        console.log('error =>', error);
-      })
+        .then(response => {
+          req.user = response.data
+          next()
+        })
+        .catch(error => {
+          console.log('error =>', error)
+        })
     //   requestify.post(`${baseUrl}/${userInfo}`, {},
     //   //   { cache: {
     // 	//   cache: true, // Will set caching to true for this request.
@@ -42,5 +49,5 @@ module.exports = {
     //   console.log('erorr =>', erorr);
     // });
     }
-  },
+  }
 }
